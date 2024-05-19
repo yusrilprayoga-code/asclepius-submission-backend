4ef6debd0811db6ed7986c6abd88c05b864dae05
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-05-19 15:44:45 +0700

    Update Server

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const InputError = require('../exceptions/InputError');
  
 (async () => {
     const server = Hapi.server({
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 5000,
         host: '0.0.0.0',
         routes: {
             cors: {
